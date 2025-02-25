f6b2058e3a0aba710b85d4b24fa430f5e37b64ff
littlekimber <zhyan0096@gmail.com>
2025-02-24 20:21:57 -0600

    rbac setup

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,8 @@ from flask_cors import CORS
 import config
 from tests.test_routes import test_group, test_person
 import secrets
+from flask_rbac import RBAC
+
 
 naming_convention = {
     "ix": 'ix_%(column_0_label)s',
@@ -32,6 +34,8 @@ db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 ma = Marshmallow()
 oidc = None
+# Role Based Access Control
+rbac = None
 
 def create_app(config_name="development"):
     app = Flask(__name__)
@@ -49,6 +53,8 @@ def create_app(config_name="development"):
     app.config["OIDC_CLIENT_SECRETS"] = "./client_secrets.json"
     global oidc
     oidc = OpenIDConnect(app)
+    global rbac
+    rbac = RBAC(app)
     CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
     # Register routes (from routes.py)
     from routes.main_routes import main
--- routes/login_routes.py
+++ routes/login_routes.py
@@ -12,6 +12,7 @@ login_bp = Blueprint('login', __name__)
 def after_auth_handler(sender, **kwargs):
     """Triggered when the user is authenticated. Check if user is already registered"""
     email = session["oidc_auth_profile"]["email"]
+    print(email)
     try:
         Person.query.filter_by(email=email).first_or_404(description="User not found")
         return redirect("http://localhost:5173")
@@ -49,4 +50,3 @@ def test():
     return jsonify({"message": "hello"})
 
 
-# https://dev-flo54hw1p0ohwfvo.us.auth0.com/oidc/logout?id_token_hint=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Im55Wnd0MmJTQnQzbmc0UjJleTVLaiJ9.eyJlbWFpbCI6InpoeWFuMDA5NkBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6Ly9kZXYtZmxvNTRodzFwMG9od2Z2by51cy5hdXRoMC5jb20vIiwiYXVkIjoieGRtSldtZFFXcTd6d0VOalBORnZmMHZYVXdHT3hiR00iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDEwODc1NjQzMTk4ODYyNDU2MjY5OSIsImlhdCI6MTc0MDA4MjY5MywiZXhwIjoxNzQwMTE4NjkzLCJzaWQiOiJYeUpJWnMtWXZkT1Y5c25MXzB0eUJIVHpVNkpWUkhOZiIsIm5vbmNlIjoiZXJGWkRqY1ZJRHFKSmxtOFh2aWYifQ.JtRn8UvwRCkAjPDCOAfnsaw2UohFLPglRvTCJflWMZgOvU_rwrTMKBAx09-jb2v7qfJFQL-wCMTbT0HLsZrMOm81A2JHXKJhyQEoNrW43q6KwJ6sna5Cs9bvx-l1FV9t3riImVUFaGm4nPtWG4qzo96qW4DFl8Deoow6g2EJ-gnCuEHY0pzSkjp1KesHd-dk_lajT2lFgdyqGtxXwz6DZgdlYMg4jm64bjUqjUmb5s1QSBqsJYqBAt8IFJhHW76RCn4l8Xtmm5NJXvfmW-QeGVELd23PowyzmK3s9sU_-QWXKBi2TVQQfWGAD-l0J_-TsVBkO6wJrAFSL9edkGLm7g&post_logout_redirect_uri=http://localhost:5173
