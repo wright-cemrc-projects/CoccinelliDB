200ee812dde6ac741c0fb6234965f71cfad33e28
YAN ZHUANG <yzhuang63@BIOCNB-01445M.local>
2025-04-23 12:31:46 -0500

    users without roles will only be able to view dashboard

--- client/src/config/resources.tsx
+++ client/src/config/resources.tsx
@@ -135,6 +135,9 @@ const roleAccessMap: Record<string, string[]> = {
 };
 
 export const filterResourcesByRoles = (roles: string[]): IResourceItem[] => {
+    if (roles.length === 0) {
+        return resources.filter(resource => resource.name === "dashboard");
+    }
     roles = roles.map((e) => e.toLowerCase());
     if (roles.includes("admin")) {
         return resources; // full access
