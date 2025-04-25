f30ef87b1611b6ef0e0b7a7b1c5952d0408fc46b
Matt Larson <larsonmattr@gmail.com>
2025-04-25 16:12:35 -0500

    Restore back the /me route.

--- routes/login_routes.py
+++ routes/login_routes.py
@@ -52,20 +52,19 @@ def me():
     :return: User info
     """
 
-    # user_info = oidc.user_getinfo(['email', 'sub', 'name'])
-    # print(user_info, file=sys.stderr)
-    # person = Person.query.filter_by(email=user_info["email"]).first()
-    # if not person:
-    #      # print(f"{user_info['name']} with {user_info['email']} not registered", file=sys.stderr)
-    #      return jsonify({"error": "Person not found"}), 404
-    # roles = [role.name for role in person.roles]
-    # # print(roles)
-    # user_dict = {
-    #     "email": user_info['email'],
-    #     "emailmd5": hashlib.md5(user_info['email'].encode('utf-8')).hexdigest(),
-    #     "roles": roles
-    # }
-    #
-    # return jsonify(user_dict)
-    return jsonify({"email": "zhyan0096@gmail.com", "roles": ["Admin"]})
+    user_info = oidc.user_getinfo(['email', 'sub', 'name'])
+    print(user_info, file=sys.stderr)
+    person = Person.query.filter_by(email=user_info["email"]).first()
+    if not person:
+         # print(f"{user_info['name']} with {user_info['email']} not registered", file=sys.stderr)
+         return jsonify({"error": "Person not found"}), 404
+    roles = [role.name for role in person.roles]
+    # print(roles)
+    user_dict = {
+        "email": user_info['email'],
+        "emailmd5": hashlib.md5(user_info['email'].encode('utf-8')).hexdigest(),
+        "roles": roles
+    }
+   
+    return jsonify(user_dict)
 
